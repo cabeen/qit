8f4d3105bc8e1aceded2c31a6d8ac9d060a23e6c
shawnxz <42953749+shawnxz@users.noreply.github.com>
2023-04-02 15:59:46 -0400

    Update qitview.bat
    Relocate java.exe in the Windows version

--- bin/qitview.bat
+++ bin/qitview.bat
@@ -1,1 +1,1 @@
-"%~dp0\runtime\bin\java.exe" -Xmx8G -cp "%~dp0\..\lib\jars\qit.jar" qitview.main.Viewer --verbose --debug
+"%~dp0\runtime\win\bin\java.exe" -Xmx8G -cp "%~dp0\..\lib\jars\qit.jar" qitview.main.Viewer --verbose --debug
